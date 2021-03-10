9f4250f288d1fd14d37c69964a6aab6da09b980c
shaftyel <shaftyel@users.noreply.github.com>
2021-03-09 22:47:28 -0300

    Update hostname.py

--- hostname.py
+++ hostname.py
@@ -1,5 +1,8 @@
 from subprocess import Popen, PIPE
 
+# Función que captura la salida del comando hostname y
+# lo envía a un txt
+
 
 def capturar_comando():
     proceso = Popen("hostname", shell=True, stdout=PIPE)
@@ -7,19 +10,27 @@ def capturar_comando():
     comando = open("captura.txt", "wb")
     for listado in proceso.stdout.readlines():
         comando.write(listado)
-        print(listado)
     comando.close()
 
     proceso.stdout.close()
 
+# Función que lee la salida almacenada en el txt,
+# la almacena en una variable, la cual es utilizada
+# en un string de comandos para cambiar el hostname
+
 
 def leer_captura():
+    # Solicitamos el ingreso del nuevo hostname
     hostname_new = input('Ingrese el nuevo hostname: ')
+    # Buscamos el actual hostname y lo almacenamos en variable
     f = open('captura.txt', 'r')
-    hostname_old = f.read()
-    proceso = Popen(
-        "wmic computersystem where caption='"+hostname_old+"'rename "+hostname_new, shell=True, stdout=PIPE)
-
+    hostname_old = f.readlines(1)
+    f.close()
+    # se agregan ambas variables al comando wmic
+    # para cambio de hostname
+    proceso = Popen("wmic computersystem where caption='"+hostname_old[0].rstrip(
+        '\n')+"' rename "+hostname_new, shell=True, stdout=PIPE)
+    # Se almacena la salida del comando ejecutado
     comando = open("captura2.txt", "wb")
     for listado in proceso.stdout.readlines():
         comando.write(listado)
@@ -27,7 +38,6 @@ def leer_captura():
     comando.close()
 
     proceso.stdout.close()
-    f.close()
 
 
 capturar_comando()
